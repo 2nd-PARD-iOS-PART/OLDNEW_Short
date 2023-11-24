4f9626b3a849e14867043326548b7fcae88a51f9
LeeShinwon <leeshin1130@gmail.com>
2023-11-25 01:04:02 +0900

    create letter model

---
+++ Short iOS/DataModel/LetterModel.swift
@@ -0,0 +1,18 @@
+//
+//  LetterModel.swift
+//  Short iOS
+//
+//  Created by 이신원 on 2023/11/25.
+//
+
+import UIKit
+
+struct LetterModel : Codable {
+    let title : String
+    let content: String
+    let writer: String
+    let dateTime: Date
+    let imgUrl: String
+}
+//here
+
